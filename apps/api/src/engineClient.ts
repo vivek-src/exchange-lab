1b63c27fec43fb8356d623807fc51c9fefdf0681
viveek-sh <get.viveksahu@gmail.com>
2026-02-24 21:33:04 +0530

    add Redis Queue layer

---
+++ apps/api/src/engineClient.ts
@@ -0,0 +1,43 @@
+import { createClient } from "redis";
+import type { RedisClientType } from "redis";
+import { randomUUID } from "crypto";
+import type { EngineResponse } from "./types/engineResponse.js";
+
+export class EngineClient {
+  private client: RedisClientType;
+  private publisher: RedisClientType;
+  private static instance: EngineClient;
+
+  private constructor() {
+    this.client = createClient();
+    this.client.connect();
+    this.publisher = createClient();
+    this.publisher.connect();
+  }
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new EngineClient();
+    }
+    return this.instance;
+  }
+  public sendRequest(message: any) {
+    return new Promise<EngineResponse>((resolve, reject) => {
+      const id = randomUUID();
+
+      const timeout = setTimeout(() => {
+        this.client.unsubscribe(id);
+        reject(new Error("Engine timeout"));
+      }, 7000);
+
+      this.client.subscribe(id, (message) => {
+        clearTimeout(timeout);
+        this.client.unsubscribe(id);
+        resolve(JSON.parse(message));
+      });
+      this.publisher.lPush(
+        "messages",
+        JSON.stringify({ clientId: id, message }),
+      );
+    });
+  }
+}

219507bcf803fcba79edaca2ac3acaa08088bdcd
viveek-sh <get.viveksahu@gmail.com>
2026-07-07 12:37:00 +0530

    feat(web): add orderBook and trades component for dashboard

--- packages/web/components/heroBanner.tsx
+++ packages/web/components/heroBanner.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 import { Coins } from "lucide-react";
+import { Button } from "./ui/button";
 
 export default function HeroBanner() {
   return (
@@ -10,20 +11,20 @@ export default function HeroBanner() {
           <Coins className="size-5 stroke-2 shrink-0" />
           <span className="font-medium text-sm">
             Start trading today with a <strong>$50,000</strong> virtual balance
-            available for all new members.
+            and coindrops available for all new members.
           </span>
         </div>
       </div>
       {/* Hero Section */}
       <div className="mt-6 rounded-xl bg-gradient-to-r from-background to-muted p-8">
-        <h1 className="text-3xl font-bold">
-          Earn 5.16% APY on your SOL collateral
-        </h1>
+        <h1 className="text-3xl font-bold">Trade on RIL</h1>
         <p className="mt-2 max-w-xl text-muted-foreground">
           Lend SOL to earn staking yield + lending yield, and use as collateral.
         </p>
 
-        <Button className="mt-6">Lend SOL</Button>
+        <Link href="/trade/RIL_INR">
+          <Button className="mt-6">Trade RIL</Button>
+        </Link>
       </div>
     </>
   );
